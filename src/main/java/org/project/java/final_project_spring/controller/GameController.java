d377b3ffff024096aa3383f0f2725abcc06d58c3
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-26 11:51:47 +0200

    Add show view and update navigation; implement game detail retrieval in GameController

--- src/main/java/org/project/java/final_project_spring/controller/GameController.java
+++ src/main/java/org/project/java/final_project_spring/controller/GameController.java
@@ -1,6 +1,7 @@
 package org.project.java.final_project_spring.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.project.java.final_project_spring.model.Game;
 import org.project.java.final_project_spring.repository.GameRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +26,15 @@ public class GameController {
         model.addAttribute("games", games);
 
         return "games/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Optional<Game> game = gameRepository.findById(id);
+        if (game.isEmpty()) {
+            throw new Error("This game doesn't exist");
+        }
+        model.addAttribute("game", game);
+        return "games/show";
     }
 }
