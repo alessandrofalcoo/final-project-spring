fbde8d350d0252ad805213a30170c780c8947a8d
alessandrofalcoo <alefalco.1003@gmail.com>
2025-09-02 13:49:24 +0200

    Refactor GameRestController: change from Controller to RestController, update index method to accept pagination parameters, and improve response handling for game retrieval.

--- src/main/java/org/project/java/final_project_spring/controller/GameRestController.java
+++ src/main/java/org/project/java/final_project_spring/controller/GameRestController.java
@@ -6,15 +6,17 @@ import org.project.java.final_project_spring.model.Game;
 import org.project.java.final_project_spring.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/games")
 public class GameRestController {
 
@@ -22,7 +24,9 @@ public class GameRestController {
     private GameService gameService;
 
     @GetMapping
-    public Page<Game> index(Pageable pageable) {
+    public Page<Game> index(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
         Page<Game> games = gameService.findAll(pageable);
         return games;
     }
@@ -34,6 +38,6 @@ public class GameRestController {
             return new ResponseEntity<Game>(HttpStatusCode.valueOf(404));
         }
 
-        return new ResponseEntity<Game>(HttpStatusCode.valueOf(200));
+        return new ResponseEntity<Game>(gameAttempt.get(), HttpStatusCode.valueOf(200));
     }
 }
