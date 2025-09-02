7aa0fcd1fb26d2828095ffbc0e94a1ab05ff535a
alessandrofalcoo <alefalco.1003@gmail.com>
2025-09-02 14:42:16 +0200

    Refactor GameRestController: add store, update, and delete methods for game management, enhancing RESTful API functionality.

--- src/main/java/org/project/java/final_project_spring/controller/GameRestController.java
+++ src/main/java/org/project/java/final_project_spring/controller/GameRestController.java
@@ -10,8 +10,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +43,30 @@ public class GameRestController {
         }
 
         return new ResponseEntity<Game>(gameAttempt.get(), HttpStatusCode.valueOf(200));
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> store(@RequestBody Game game) {
+        Game newGame = gameService.create(game);
+        return new ResponseEntity<Game>(newGame, HttpStatusCode.valueOf(200));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> update(@RequestBody Game game, @PathVariable Integer id) {
+        if (gameService.findById(id).isEmpty()) {
+            return new ResponseEntity<Game>(HttpStatusCode.valueOf(404));
+        }
+        game.setId(id);
+        Game modifiedGame = gameService.update(game);
+        return new ResponseEntity<Game>(modifiedGame, HttpStatusCode.valueOf(200));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Game> delete(@PathVariable Integer id) {
+        if (gameService.findById(id).isEmpty()) {
+            return new ResponseEntity<Game>(HttpStatusCode.valueOf(404));
+        }
+        gameService.deleteById(id);
+        return new ResponseEntity<Game>(HttpStatusCode.valueOf(200));
     }
 }
