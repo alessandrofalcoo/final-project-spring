2750ffb3a56bac5d52ceb0d052c3cb963f8a868c
alessandrofalcoo <alefalco.1003@gmail.com>
2025-09-03 12:24:43 +0200

    Implement additional UserDetails methods: add account status checks for non-expired, non-locked, and enabled states in DatabaseUserDetails.

--- src/main/java/org/project/java/final_project_spring/security/DatabaseUserDetails.java
+++ src/main/java/org/project/java/final_project_spring/security/DatabaseUserDetails.java
@@ -50,4 +50,24 @@ public class DatabaseUserDetails implements UserDetails {
         return this.authorities;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
