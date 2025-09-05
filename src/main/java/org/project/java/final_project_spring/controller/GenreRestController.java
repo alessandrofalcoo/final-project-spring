e64b7af2cec638adb09bfbf10ef6c101d117aa33
alessandrofalcoo <alefalco.1003@gmail.com>
2025-09-05 15:06:54 +0200

    add CrossOrigin annotation for cors policy

--- src/main/java/org/project/java/final_project_spring/controller/GenreRestController.java
+++ src/main/java/org/project/java/final_project_spring/controller/GenreRestController.java
@@ -8,6 +8,7 @@ import org.project.java.final_project_spring.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/genre")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GenreRestController {
     @Autowired
     private GenreService genreService;
